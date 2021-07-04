f28e9eaa95ca83de5c7afe2c85dd0b661fd48ced
arjunkrishna <arjun.krishna@huawei.com>
2021-07-04 09:39:22 -0400

    ignore row in a if all 0 in mat mult

--- SparseMatrixMultiplication.py
+++ SparseMatrixMultiplication.py
@@ -21,9 +21,11 @@ def sparse_matrix_multiplication(matrix_a, matrix_b):
         result = [[0 for i in range(len(matrix_b[0]))] for j in range(len(matrix_a))]
         # do matrix multiplication and return
         for i in range(len(matrix_a)):
-            for j in range(len(matrix_b[0])):
-                for k in range(len(matrix_b)):
-                    result[i][j] += matrix_a[i][k] * matrix_b[k][j]
+            for k in range(len(matrix_a[0])):
+                if matrix_a[i][k] != 0:
+                    for j in range(len(matrix_b[0])):
+                        if matrix_a[i][k] != 0 and matrix_b[k][j] != 0:
+                            result[i][j] += matrix_a[i][k] * matrix_b[k][j]
 
         return result
 
